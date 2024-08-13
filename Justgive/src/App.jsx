72c13b74aec94e4ccd345740305d8850d3dd927a
Gracendo <gracendo96@gmail.com>
2024-08-13 07:00:52 -0500

    did the login interface

---
+++ Justgive/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./components/Login and Registration/Login"
+
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ Justgive/src/components/Login and Registration/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+/* importing css */
+import "../../assets/css/Login.css";
+/*import of icons */
+
+import { FaEnvelope } from "react-icons/fa";
+import { FaLock } from "react-icons/fa6";
+
+/*import of image */
+import Child from "../../assets/images/register_form_img_child.jpg";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="main-login-form-conatiner">
+        <div className="second-main-login-form-container">
+          <div className="sub-picture-login-conatiner">
+            <h2>Welcome Back</h2>
+            <p>Let's make the world a better place</p>
+          </div>
+          <div className="sub-form-login-conatiner">
+            <h2>LOGIN</h2>
+            <form action="" method="POST">
+              <div className="input-field">
+                <input type="text" placeholder="Email" /> <FaEnvelope className="icon" />
+              </div>
+              <div className="input-field">
+                <input type="text" placeholder="Password" /> <FaLock className="icon" />
+              </div>
+              <button type="submit" className="login-form-login-button">
+                {" "}
+                Login{" "}
+              </button>
+            </form>
+            <div className="bottom-link">
+              Don't have an account?
+              <a href="#">Signup</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
